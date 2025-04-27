c89b381368e0d253c4fbd8d9532d3ae4603d6d95
JunedSetiawan <junedhayko502@gmail.com>
2025-04-27 19:46:06 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -8,29 +8,14 @@ const nextConfig = {
     compiler: {
         removeConsole: process.env.NODE_ENV !== "development"     // Remove console.log in production
     },
-    // Add headers configuration
-//    async headers() {
-//     return [
-//         {
-//             source: '/:path*',
-//             headers: [
-//                 {
-//                     key: 'Content-Security-Policy',
-//                     // Using a different directive that allows mixed content
-//                     value: 'block-all-mixed-content',
-//                 },
-//             ],
-//         },
-//     ];
-// },
- async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://45.13.132.248:8080/api/:path*',
-            },
-        ];
-    },
+    async rewrites() {
+            return [
+                {
+                    source: '/api/:path*',
+                    destination: `${process.env.BACKEND_API_URL}/:path*`,
+                },
+            ];
+        },
 };
 
 export default withPWA({
